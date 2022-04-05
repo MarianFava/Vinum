1b44112a0bf2934b2a19ea59d6fc2d2e99b84d78
MarianFava <marianaspano@gmail.com>
2022-04-05 19:06:31 -0300

    Incorporacion de cards en html mediante manejo del DOM

--- scripts/main.js
+++ scripts/main.js
@@ -2,40 +2,41 @@
 const productos = [];
 //Declaración de la clase
 class Producto {
-    constructor (codigo, marca, tipo, origen, precio, imagen){
+    constructor (codigo, marca, tipo, variedad, origen, precio, imagen){
         this.codigo = codigo;
         this.marca = marca;
         this.tipo = tipo;
+        this.variedad = variedad;
         this.origen = origen;
         this.precio = precio;
         this.imagen = imagen;
     }
 }
 //Con el método push incorporamos los nuevos objetos de tipo "Producto" al Array productos
-productos.push(new Producto("V001","Chateau Cordillera","Malbec","Montecaseros,Mendoza",1220,"./media/ChateuMalbec.jpeg"));
-productos.push(new Producto("V002","Chateau Cordillera","Cabernet","Montecaseros,Mendoza",1220,"./media/ChateuCabSauv.jpeg"));
-productos.push(new Producto("V003","Caminante","Chardonnay","Valle de Uco",820,"./media/CaminanteChardonnay.jpeg"));
-productos.push(new Producto("V004","Azul","Malbec","Agua Amarga,Tupungato",610,"./media/AzulMalbec.jpeg"));
-productos.push(new Producto("V005","Acordeon","Malbec","Gualtallary,Mendoza",820,"./media/AcordeonMalbec.jpeg"));
-productos.push(new Producto("V006","Chateau Cordillera","Cabernet Franc","Mendoza",1220,"./media/ChateuCabFranc.jpeg"));
-productos.push(new Producto("V007","Caminante","Cabernet Franc","Mendoza",820,"./media/CaminanteCabFranc.jpeg"));
-productos.push(new Producto("V008","Caminante","Malbec","Mendoza",830,"./media/CaminanteMalbec.jpeg"));
-productos.push(new Producto("G001","Aconcagua","Dry","Argentina",2100,"./media/Gin_Aconcagua.jpeg"));
-productos.push(new Producto("G002","Brighton","London Dry","Argentina",1020,"./media/GinBrighton.jpeg"));
-productos.push(new Producto("G003","Bombay Bramble","Bramble","Inglaterra",4400,"./media/GinBombayRed.jpeg"));
-productos.push(new Producto("G004","Gordons","London Dry","Inglaterra",2200,"./media/GinGordons.jpeg"));
-productos.push(new Producto("G005","Bombay Sapphire","Dry","Inglaterra",3500,"./media/GinBombayAzul.jpeg"));
-productos.push(new Producto("GOO6","Kunuk Citrico","London Dry","Argentina",2900,"./media/GinKunuk.jpeg"));
-productos.push(new Producto("G007","Merle","London Dry","Argentina",1220,"./media/GinMerle.jpeg"));
-productos.push(new Producto("G008","Beefeater","London Dry","Inglaterra",5750,"./media/Gin_Beefeater.jpeg"));
-productos.push(new Producto("W001","Ballantines","Scotch","Escocia",2600,"./media/WhiskeyBallantines.jpeg"));
-productos.push(new Producto("W002","Jack Daniels","Bourbon","EEUU",4700,"./media/WhiskeyJackDaniels.jpeg"));
-productos.push(new Producto("W003","Johnny Walker Gold","Scotch","Escocia",8500,"./media/JohnnyGoldLabel.jpeg"));
-productos.push(new Producto("W004","Jim Bean","Bourbon","EEUU",5000,"./media/WhiskeyJimBean.jpeg"));
-productos.push(new Producto("WOO5","Jim Bean Honey","Bourbon","EEUU",4300,"./media/WhiskeyJimBeanHoney.jpeg"));
-productos.push(new Producto("W006","Johnny Walker Black","Scotch","Escocia",5300,"./media/WhiskeyJohnnyBlack.jpeg"));
-productos.push(new Producto("W007","Johnny Walker Red","Scotch","Escocia",2700,"./media/WhiskeyJohnnyRed.jpeg"));
-productos.push(new Producto("W008","Variedades Jim Bean","Scotch","EEUU",11000,"./media/WhiskeyJimBean.jpeg"));
+productos.push(new Producto("V001","Chateau Cordillera","Vino","Malbec","Montecaseros,Mendoza",1220,"./media/ChateuMalbec.jpeg"));
+productos.push(new Producto("V002","Chateau Cordillera","Vino","Cabernet","Montecaseros,Mendoza",1220,"./media/ChateuCabSauv.jpeg"));
+productos.push(new Producto("V003","Caminante","Vino","Chardonnay","Valle de Uco",820,"./media/CaminanteChardonnay.jpeg"));
+productos.push(new Producto("V004","Azul","Vino","Malbec","Agua Amarga,Tupungato",610,"./media/AzulMalbec.jpeg"));
+productos.push(new Producto("V005","Acordeon","Vino","Malbec","Gualtallary,Mendoza",820,"./media/AcordeonMalbec.jpeg"));
+productos.push(new Producto("V006","Chateau Cordillera","Vino","Cabernet Franc","Mendoza",1220,"./media/ChateuCabFranc.jpeg"));
+productos.push(new Producto("V007","Caminante","Vino","Cabernet Franc","Mendoza",820,"./media/CaminanteCabFranc.jpeg"));
+productos.push(new Producto("V008","Caminante","Vino","Malbec","Mendoza",830,"./media/CaminanteMalbec.jpeg"));
+productos.push(new Producto("G001","Aconcagua","Gin","Dry","Argentina",2100,"./media/Gin_Aconcagua.jpeg"));
+productos.push(new Producto("G002","Brighton","Gin","London Dry","Argentina",1020,"./media/GinBrighton.jpeg"));
+productos.push(new Producto("G003","Bombay Bramble","Gin","Bramble","Inglaterra",4400,"./media/GinBombayRed.jpeg"));
+productos.push(new Producto("G004","Gordons","Gin","London Dry","Inglaterra",2200,"./media/GinGordons.jpeg"));
+productos.push(new Producto("G005","Bombay Sapphire","Gin","Dry","Inglaterra",3500,"./media/GinBombayAzul.jpeg"));
+productos.push(new Producto("GOO6","Kunuk Citrico","Gin","London Dry","Argentina",2900,"./media/GinKunuk.jpeg"));
+productos.push(new Producto("G007","Merle","Gin","London Dry","Argentina",1220,"./media/GinMerle.jpeg"));
+productos.push(new Producto("G008","Beefeater","Gin","London Dry","Inglaterra",5750,"./media/Gin_Beefeater.jpeg"));
+productos.push(new Producto("W001","Ballantines","Whiskey","Scotch","Escocia",2600,"./media/WhiskeyBallantines.jpeg"));
+productos.push(new Producto("W002","Jack Daniels","Whiskey","Bourbon","EEUU",4700,"./media/WhiskeyJackDaniels.jpeg"));
+productos.push(new Producto("W003","Johnny Walk. Gold","Whiskey","Scotch","Escocia",8500,"./media/JohnnyGoldLabel.jpeg"));
+productos.push(new Producto("W004","Jim Bean","Whiskey","Bourbon","EEUU",5000,"./media/WhiskeyJimBean.jpeg"));
+productos.push(new Producto("WOO5","Jim Bean Honey","Whiskey","Bourbon","EEUU",4300,"./media/WhiskeyJimBeanHoney.jpeg"));
+productos.push(new Producto("W006","Johnny Walk. Black","Whiskey","Scotch","Escocia",5300,"./media/WhiskeyJohnnyBlack.jpeg"));
+productos.push(new Producto("W007","Johnny Walker Red","Whiskey","Scotch","Escocia",2700,"./media/WhiskeyJohnnyRed.jpeg"));
+productos.push(new Producto("W008","Variedades Jim Bean","Whiskey","Scotch","EEUU",11000,"./media/WhiskeyJimBean.jpeg"));
 
 
 
@@ -79,8 +80,8 @@ alert("El total de su compra es: $" + mostrarTotal);
 
 //Creación de cards manipulando el DOM
 
-function dibujarCard(productos){
-    let seccionProductos = document.getElementsByClassName("cardsCatalogo");
+function dibujarCard(productos, catalogo){
+    let seccionProductos = document.getElementById(catalogo);
 
     let card = document.createElement("div");
     card.classList.add("card");
@@ -92,11 +93,18 @@ function dibujarCard(productos){
     <div class="card-body">
     <h5 class="card-title">${productos.marca.toUpperCase()}</h5>
     <p class="card-text"> Tipo:${productos.tipo} <br/> Origen:${productos.origen}
-    <br/> Código:${productos.codigo} <br/> Precio:${productos.precio} </p>
+    <br/> Código:${productos.codigo} <br/> <span class="precio"> Precio:$ ${productos.precio} </span> </p>
     <a href="#" class="btn btn-primary">Agregar al carrito</a>
     `;
     seccionProductos.appendChild(card);
 }
-for(const dato of productos){
-    dibujarCard(dato);
+//Se recorre el array productos con For y se filtra por tipo de bebida utilizando metodo filter
+for(const dato of productos.filter(producto => producto.tipo == "Vino")){
+    dibujarCard(dato, "catalogoVinos");
+}
+for(const dato of productos.filter(producto => producto.tipo == "Gin")){
+     dibujarCard(dato, "catalogoGin");
+}
+for(const dato of productos.filter(producto => producto.tipo == "Whiskey")){
+    dibujarCard(dato, "catalogoWhiskey");
 }
