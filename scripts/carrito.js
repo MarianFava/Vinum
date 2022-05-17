7ca198330328118e911f3fad4d7337fdde222b3c
MarianFava <marianaspano@gmail.com>
2022-05-17 20:31:19 -0300

    Entrega Final Js

--- scripts/carrito.js
+++ scripts/carrito.js
@@ -1,28 +1,22 @@
 //Se obtiene carrito del Storage y si no existe se declara vacío
 //Simplificando el código con el operador OR
-const carrito = JSON.parse(localStorage.getItem("Carrito Storage")) || [];
+let carrito = JSON.parse(localStorage.getItem("Carrito Storage")) || [];
 
 function agregarAlCarrito(evento){
-    // alert("Se agrego al carrito")
-    let codigo = evento.target.parentNode.children[0].id;
+
+    let codigo = evento.target.id;
+
     let productoSeleccionado= productos.find((prod)=> prod.codigo == codigo );
-    let encontrarProducto = carrito.find((producto)=> producto.codigo == codigo );
-    //console.log(encontrarProducto);
-    if (!encontrarProducto) {
+    let indExistenteCarrito = carrito.findIndex((producto)=> producto.codigo == codigo );
+
+    if (indExistenteCarrito == -1) {
         carrito.push({...productoSeleccionado,cantidad: 1});
     }else{
-        const idFiltrados = carrito.filter(item => item.codigo != codigo)
-        let nuevaCantidad = encontrarProducto.cantidad + 1;
-        encontrarProducto ={...encontrarProducto, cantidad: nuevaCantidad};
-        for (let i = 0; i <= carrito.length; i++) {
-             carrito.pop();
-        }
-        carrito.push(...idFiltrados);
-        carrito.push(encontrarProducto);
+        carrito[indExistenteCarrito].cantidad++;
     }
-    console.log(carrito);
     guardarCarrito();
     dibujarCarrito();
+//Implementando libreria Toastify
     Toastify({
         text: "Se agregó al carrito",
         duration: 3000,
@@ -37,23 +31,126 @@ function agregarAlCarrito(evento){
     }).showToast();
 
  }
+
 function dibujarCarrito(){
-    document.getElementById("contador-carrito").innerHTML=carrito.length;
+    let cantidadProductos = 0;
+     //Sumatoria de cantidades de productos agregados
+    carrito.forEach(itemCarrito =>{
+        cantidadProductos = cantidadProductos + itemCarrito.cantidad;
+    });
+    //Actualizacion de contador de productos de carrito
+    document.getElementById("contador-carrito").innerHTML=cantidadProductos;
+    //Se obtiene cuerpo de modal
+    let cuerpoModal = document.getElementById("cuerpoModal");
+    cuerpoModal.innerHTML="";
+    //Se crea el contenedor donde irá del detalle del carrito en el modal
+    let container = document.createElement("div");
+    container.classList.add("container");
+
+    //Titulo de la lista de productos en el carrito
+    container.innerHTML = `<div class="row">
+    <div class="col">
+    </div>
+    <div class="col">
+      Producto
+    </div>
+    <div class="col">
+      Cantidad
+    </div>
+    <div class="col">
+      P.Unitario
+    </div>
+    <div class="col">
+      Sub Total
+    </div>
+  </div>`;
+  let total = 0;
+  //Se recorre el array de productos del carrito y se dibuja en el cuerpo del modal
+  carrito.forEach((itemCarrito,index) => {
+    total = total+itemCarrito.cantidad * itemCarrito.precio;
+    container.innerHTML = container.innerHTML +
+        `<hr><div class="row">
+            <div class="col">
+                <img class="imgCarrito" src="${itemCarrito.imagen}"/>
+            </div>
+        <div class="col">
+            ${itemCarrito.marca}-${itemCarrito.variedad}
+        </div>
+        <div class="col">
+            <a href="#" id="${index}" class="btn btn-outline-info btn-sm btnInc">+</a>
+            ${itemCarrito.cantidad}
+            <a href="#" id="${index}" class="btn btn-outline-info btn-sm btnDec">-</a>
+        </div>
+        <div class="col">$
+            ${itemCarrito.precio}
+        </div>
+        <div class="col">$
+            ${itemCarrito.cantidad * itemCarrito.precio}
+        </div>
+        </div>`;
+    });
+
+    container.innerHTML = container.innerHTML +
+        `<hr>
+        <div class="row ">
+            <div class="col-8">TOTAL: </div>
+            <div class="col">$${total}</div>
+        </div>`;
+
+    //Se incorpora el contenedor con el detalle en el cuerpo del modal
+    cuerpoModal.appendChild(container);
+
+    //Ahora que ya está dibujado el contenedor dentro del cuerpo del modal
+    //Se capturan los eventos de incremento y decremento de cantidad de cada producto
+    //EVENTO INCREMENTAR
+    document.querySelectorAll('.btnInc').forEach(boton=>{
+        boton.addEventListener('click',(e)=>{
+            carrito[e.target.id].cantidad++;
+            dibujarCarrito();
+            guardarCarrito();
+        })
+    });
+
+    //EVENTO DECREMENTAR
+    document.querySelectorAll('.btnDec').forEach(boton=>{
+        boton.addEventListener('click',(e)=>{
+            carrito[e.target.id].cantidad--;
+            if(carrito[e.target.id].cantidad == 0){
+                carrito.splice(e.target.id,1);
+            }
+            dibujarCarrito();
+            guardarCarrito();
+        })
+    });
+
 }
- //Storage
- function guardarCarrito(){
- //Local Storage y JSON
-     localStorage.setItem("Carrito Storage", JSON.stringify(carrito));
- }
-
- //EVENTO Click
-
-let boton = document.getElementsByClassName("btn btn-primary");
-let numBoton = boton.length;
-
-for (let i = 0; i < numBoton; i++) {
-    boton[i].addEventListener('click', agregarAlCarrito, false);
+//Storage
+function guardarCarrito(){
+//Local Storage y JSON
+    localStorage.setItem("Carrito Storage", JSON.stringify(carrito));
 }
+
 dibujarCarrito();
 
+//EVENTO CLICK EN CARRITO
+document.getElementById("btnConfirmar").addEventListener('click',()=>{
+    if (carrito.length>0){
+        Swal.fire({
+            title: 'Elegi el medio de pago',
+            showDenyButton: true,
+            showCancelButton: true,
+            confirmButtonText: 'Debito/Credito',
+            denyButtonText: `Mercado Pago`,
+          }).then((result) => {
+            if (result.isConfirmed) {
+              Swal.fire('Gracias por tu compra!', '', 'success')
+            } else if (result.isDenied) {
+              Swal.fire('Gracias por tu compra', '', 'info')
+            }
+          })
+        carrito.splice(0,carrito.length);
+        dibujarCarrito();
+        guardarCarrito();
+    }
+});
 
--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,8 @@
 Swal.fire({
     title: 'Bienvenido a Vinum',
     text: "Es mayor de 18 años?",
+    color:'#f50792',
+    background: '#383838',
     showCancelButton: true,
     confirmButtonColor: '#40DFEF',
     cancelButtonColor: '#FF6363',
@@ -19,7 +21,7 @@ Swal.fire({
     }
   })
 //Declaración del array productos
-const productos = [];
+let  productos = [];
 //Declaración de la clase
 class Producto {
     constructor (codigo, marca, tipo, variedad, origen, precio, imagen){
@@ -32,37 +34,24 @@ class Producto {
         this.imagen = imagen;
     }
 }
-//Con el método push incorporamos los nuevos objetos de tipo "Producto" al Array productos
-productos.push(new Producto("V001","Chateau Cordillera","Vino","Malbec","Montecaseros,Mendoza",1220,"./media/ChateuMalbec.jpeg"));
-productos.push(new Producto("V002","Chateau Cordillera","Vino","Cabernet","Montecaseros,Mendoza",1220,"./media/ChateuCabSauv.jpeg"));
-productos.push(new Producto("V003","Caminante","Vino","Chardonnay","Valle de Uco",820,"./media/CaminanteChardonnay.jpeg"));
-productos.push(new Producto("V004","Azul","Vino","Malbec","Agua Amarga,Tupungato",610,"./media/AzulMalbec.jpeg"));
-productos.push(new Producto("V005","Acordeon","Vino","Malbec","Gualtallary,Mendoza",820,"./media/AcordeonMalbec.jpeg"));
-productos.push(new Producto("V006","Chateau Cordillera","Vino","Cabernet Franc","Mendoza",1220,"./media/ChateuCabFranc.jpeg"));
-productos.push(new Producto("V007","Caminante","Vino","Cabernet Franc","Mendoza",820,"./media/CaminanteCabFranc.jpeg"));
-productos.push(new Producto("V008","Caminante","Vino","Malbec","Mendoza",830,"./media/CaminanteMalbec.jpeg"));
-productos.push(new Producto("G001","Aconcagua","Gin","Dry","Argentina",2100,"./media/Gin_Aconcagua.jpeg"));
-productos.push(new Producto("G002","Brighton","Gin","London Dry","Argentina",1020,"./media/GinBrighton.jpeg"));
-productos.push(new Producto("G003","Bombay Bramble","Gin","Bramble","Inglaterra",4400,"./media/GinBombayRed.jpeg"));
-productos.push(new Producto("G004","Gordons","Gin","London Dry","Inglaterra",2200,"./media/GinGordons.jpeg"));
-productos.push(new Producto("G005","Bombay Sapphire","Gin","Dry","Inglaterra",3500,"./media/GinBombayAzul.jpeg"));
-productos.push(new Producto("G006","Kunuk Citrico","Gin","London Dry","Argentina",2900,"./media/GinKunuk.jpeg"));
-productos.push(new Producto("G007","Merle","Gin","London Dry","Argentina",1220,"./media/GinMerle.jpeg"));
-productos.push(new Producto("G008","Beefeater","Gin","London Dry","Inglaterra",5750,"./media/Gin_Beefeater.jpeg"));
-productos.push(new Producto("W001","Ballantines","Whiskey","Scotch","Escocia",2600,"./media/WhiskeyBallantines.jpeg"));
-productos.push(new Producto("W002","Jack Daniels","Whiskey","Bourbon","EEUU",4700,"./media/WhiskeyJackDaniels.jpeg"));
-productos.push(new Producto("W003","Johnny Walk. Gold","Whiskey","Scotch","Escocia",8500,"./media/JohnnyGoldLabel.jpeg"));
-productos.push(new Producto("W004","Jim Bean","Whiskey","Bourbon","EEUU",5000,"./media/WhiskeyJimBean.jpeg"));
-productos.push(new Producto("W005","Jim Bean Honey","Whiskey","Bourbon","EEUU",4300,"./media/WhiskeyJimBeanHoney.jpeg"));
-productos.push(new Producto("W006","Johnny Walk. Black","Whiskey","Scotch","Escocia",5300,"./media/WhiskeyJohnnyBlack.jpeg"));
-productos.push(new Producto("W007","Johnny Walker Red","Whiskey","Scotch","Escocia",2700,"./media/WhiskeyJohnnyRed.jpeg"));
-productos.push(new Producto("W008","Variedades Jim Bean","Whiskey","Scotch","EEUU",11000,"./media/WhiskeyJimBean.jpeg"));
 
 //Llamada asincrónica
 fetch("/data/productos.json")
     .then(response => response.json())
-    .then(json => console.log(json));
-    
+    .then(json => {
+        productos = [...json];
+        //Se recorre el array productos con For y se filtra por tipo de bebida utilizando metodo filter
+        for(const dato of productos.filter(producto => producto.tipo == "Vino")){
+            dibujarCard(dato, "catalogoVinos");
+        }
+        for(const dato of productos.filter(producto => producto.tipo == "Gin")){
+            dibujarCard(dato, "catalogoGin");
+        }
+        for(const dato of productos.filter(producto => producto.tipo == "Whiskey")){
+            dibujarCard(dato, "catalogoWhiskey");
+        }
+    });
+
 //Acceso condicional a un objeto
 console.log(productos?.calificacion || "La calificación no esta definida");
 //Desestructuración de un objeto
@@ -97,21 +86,13 @@ function dibujarCard(productos, catalogo){
     class="card-img-top"
     alt="${productos.marca}"/>
     <div class="card-body">
-    <h5 id="${productos.codigo}" class="card-title">${productos.marca.toUpperCase()}</h5>
+    <h5  class="card-title">${productos.marca.toUpperCase()}</h5>
     <p class="card-text"> Tipo:${productos.tipo} <br/> Origen:${productos.origen}
-    <br/> Código:${productos.codigo} <br/> <span class="precio"> Precio:$ ${productos.precio} </span> </p>
-    <a href="#" class="btn btn-primary">Agregar al carrito</a> </div>
+    <br/> Variedad:${productos.variedad} <br/> <span class="precio"> Precio:$ ${productos.precio} </span> </p>
+    <a href="#" id="${productos.codigo}" class="btn btn-primary">Agregar al carrito</a> </div>
     `;
     seccionProductos.appendChild(card);
+    document.getElementById(productos.codigo).addEventListener('click', agregarAlCarrito, false);
 }
 
-//Se recorre el array productos con For y se filtra por tipo de bebida utilizando metodo filter
-for(const dato of productos.filter(producto => producto.tipo == "Vino")){
-    dibujarCard(dato, "catalogoVinos");
-}
-for(const dato of productos.filter(producto => producto.tipo == "Gin")){
-     dibujarCard(dato, "catalogoGin");
-}
-for(const dato of productos.filter(producto => producto.tipo == "Whiskey")){
-    dibujarCard(dato, "catalogoWhiskey");
-}
+
